0cfb705abf2d86b162467cdc41c9f9cb40bd7e72
Stephan Fowler <stephanfowler@gmail.com>
2015-11-17 16:01:23 +0000

    Toggle meganav

--- chrome-extension/inject-nav.js
+++ chrome-extension/inject-nav.js
@@ -48,7 +48,8 @@ function capitalize(id) {
 function loadItems() {
     var xhr = new XMLHttpRequest(),
         recents,
-        collections;
+        collections,
+        shown = true;
 
     document.querySelector('#header').insertAdjacentHTML('afterBegin',
         '<style>' +
@@ -102,7 +103,7 @@ function loadItems() {
                 'transition-delay: 1s;' +
             '}' +
 
-            '.ojnav__front--hide .ojnav__coll__item {' +
+            '.ojnav__front--initial .ojnav__coll__item {' +
                 'transform: rotateX(90deg);' +
                 'opacity: 0.5;' +
             '}' +
@@ -148,7 +149,7 @@ function loadItems() {
                 '<svg style="fill: currentColor;" class="ojnav-logo" width="36" height="36" viewBox="0 0 36 36"><path d="M21.3 8.8c0-4.9-1.5-5.7-3.3-5.7-1.8 0-3.2.7-3.2 5.7s1.5 5.5 3.2 5.5c1.8-.1 3.3-.6 3.3-5.5m-6.5 18.8c-2.3 0-2.9 1.7-2.9 2.9 0 1.8 1.6 3.4 6.3 3.4 5.3 0 6.8-1.5 6.8-3.4 0-1.7-1.3-2.9-3.4-2.9h-6.8zM10.5 2.4C4.3 5.2 0 11.4 0 18.7c0 4.9 2 9.4 5.2 12.6V31c0-3.2 3.1-4.4 5.9-5-2.6-.6-3.9-2.5-3.9-4.4 0-2.6 2.9-4.8 4.3-5.8l-.2-.1c-2.5-1.4-4.1-3.8-4.1-7 0-2.7 1.2-4.9 3.3-6.3M36 18.8C36 11.4 31.5 5 25.1 2.3c2.1 1.4 3.4 3.5 3.5 6.3l.1.6c0 5.4-4.4 8.2-10.7 8.2-1.6 0-2.7-.1-4.1-.5-.6.4-1.1 1.1-1.1 1.8 0 .9.8 1.6 1.8 1.6h8.8c5.5 0 8.2 2.2 8.2 7.1 0 1.6-.3 3.1-1 4.3 3.3-3.4 5.4-7.9 5.4-12.9"></path></svg>' +
                 '</span>' +
             '</a>' +
-            '<div class="ojnav__front ojnav__front--hide" id="ojnav__front">' +
+            '<div class="ojnav__front ojnav__front--initial" id="ojnav__front">' +
             '</div>' +
         '</div>'
     );
@@ -181,12 +182,17 @@ function loadItems() {
             setTimeout(function() {
                 document.getElementById('ojnav__front').className = 'ojnav__front';
             }, 1);
+
+            document.querySelector(".js-navigation-toggle").addEventListener("click", function() {
+                document.querySelector('.ojnav').style.display = shown ? 'none' : 'block';
+                shown = !shown;
+            });
         }
     }
     xhr.open('GET', getJsonSource(), true);
     xhr.send();
 }
 
-//removeEls('.js-navigation-header');
-loadItems();
-
+(function() {
+    loadItems();
+})();
